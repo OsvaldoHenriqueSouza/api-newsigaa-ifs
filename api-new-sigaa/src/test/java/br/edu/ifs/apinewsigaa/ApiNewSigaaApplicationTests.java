afe42a201db5fa0f63df04f4245d8b8c896a37e4
Osvaldo Henrrique <osvaldosouza_99@hotmail.com>
2024-03-22 14:01:44 -0300

    Inicia projeto

---
+++ api-new-sigaa/src/test/java/br/edu/ifs/apinewsigaa/ApiNewSigaaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.apinewsigaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNewSigaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
