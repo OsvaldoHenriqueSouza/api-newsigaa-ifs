358d5a51e3bd707b805941a0d58f787e838e6b18
Osvaldo Henrrique <osvaldosouza_99@hotmail.com>
2024-08-16 19:57:15 -0300

    Limpando comentarios

--- api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/service/ProfessorService.java
+++ api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/service/ProfessorService.java
@@ -9,7 +9,6 @@ import br.edu.ifs.apinewsigaa.rest.Dtos.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -69,47 +68,6 @@ public class ProfessorService {
         }
     }
 
-//    {
-//        "professor": {
-//        "matricula": "PROF12345",
-//                "nome": "Glauco Luiz Rezende de Carvalho",
-//                "cpf": "009.717.570-62",
-//                "email": "glauco.carvalho@academico.ifs.edu.br",
-//                "dataNascimento": "1979-10-09",
-//                "celular": "(79)98113-0366"
-//    },
-//        "disciplinas": [
-//        {
-//            "id": 1,
-//                "nome": "Programação",
-//                "numeroCreditos": 4
-//        },
-//        {
-//            "id": 2,
-//                "nome": "Banco de Dados",
-//                "numeroCreditos": 3
-//        },
-//        {
-//            "id": 3,
-//                "nome": "Estrutura de Dados",
-//                "numeroCreditos": 4
-//        }
-//  ]
-//    }
-
-    // o metodo a seguir deve retornar o professor e as disciplinas que ele leciona
-    // o json acima é um exemplo de como deve ser o retorno
-
-//    @Query(value = """
-//            SELECT p.matricula, p.nome, p.cpf, p.email, p.dataNascimento, p.celular,
-//                d.id AS disciplinaId, d.nome AS disciplinaNome, d.numeroCredito
-//            FROM professor p
-//            INNER JOIN turma t ON p.id = t.idProfessor
-//            INNER JOIN disciplina d ON t.idDisciplina = d.id
-//            WHERE p.matricula = :matricula
-//            """, nativeQuery = true)
-
-
     @Transactional(readOnly = true)
     public ProfessorModel getProfessorPorMatricula(String matricula) {
         return professorRepository.findByMatricula(matricula)
@@ -131,106 +89,6 @@ public class ProfessorService {
 
         return professorDisciplinasDto;
     }
-
-//    {
-//        "professor": {
-//        "matricula": "PROF12345",
-//                "nome": "Glauco Luiz Rezende de Carvalho",
-//                "cpf": "009.717.570-62",
-//                "email": "glauco.carvalho@academico.ifs.edu.br",
-//                "dataNascimento": "1979-10-09",
-//                "celular": "(79)98113-0366"
-//    },
-//        "disciplinas": [
-//        {
-//            "disciplina": {
-//            "id": 1,
-//                    "nome": "Programação",
-//                    "numeroCreditos": 4
-//        },
-//            "alunos": [
-//            {
-//                "id": 1,
-//                    "nome": "Ana Paula da Silva",
-//                    "cpf": "108.887.090-21",
-//                    "email": "ana.silva@academico.ifs.edu.br",
-//                    "dataNascimento": "2000-05-12",
-//                    "celular": "(79)98765-4321",
-//                    "apelido": "Ana",
-//                    "matricula": "ALU2020001"
-//            },
-//            {
-//                "id": 2,
-//                    "nome": "Carlos Eduardo Pereira",
-//                    "cpf": "469.062.520-46",
-//                    "email": "carlos.pereira@academico.ifs.edu.br",
-//                    "dataNascimento": "1999-10-20",
-//                    "celular": "(79)99876-5432",
-//                    "apelido": "Cadu",
-//                    "matricula": "ALU2020002"
-//            }
-//      ]
-//        },
-//        {
-//            "disciplina": {
-//            "id": 2,
-//                    "nome": "Banco de Dados",
-//                    "numeroCreditos": 3
-//        },
-//            "alunos": [
-//            {
-//                "id": 3,
-//                    "nome": "Maria Fernanda Costa",
-//                    "cpf": "961.508.410-73",
-//                    "email": "maria.costa@academico.ifs.edu.br",
-//                    "dataNascimento": "2001-03-15",
-//                    "celular": "(79)98765-1234",
-//                    "apelido": "Mari",
-//                    "matricula": "ALU2020003"
-//            },
-//            {
-//                "id": 4,
-//                    "nome": "João Pedro Almeida",
-//                    "cpf": "474.468.310-06",
-//                    "email": "joao.almeida@academico.ifs.edu.br",
-//                    "dataNascimento": "1998-12-05",
-//                    "celular": "(79)99876-5432",
-//                    "apelido": "JP",
-//                    "matricula": "ALU2020004"
-//            }
-//      ]
-//        },
-//        {
-//            "disciplina": {
-//            "id": 3,
-//                    "nome": "Estrutura de Dados",
-//                    "numeroCreditos": 4
-//        },
-//            "alunos": [
-//            {
-//                "id": 2,
-//                    "nome": "Carlos Eduardo Pereira",
-//                    "cpf": "801.879.720-01",
-//                    "email": "carlos.pereira@academico.ifs.edu.br",
-//                    "dataNascimento": "1999-10-20",
-//                    "celular": "(79)99876-5432",
-//                    "apelido": "Cadu",
-//                    "matricula": "ALU2020002"
-//            },
-//            {
-//                "id": 5,
-//                    "nome": "Luiza Martins",
-//                    "cpf": "460.454.960-51",
-//                    "email": "luiza.martins@academico.ifs.edu.br",
-//                    "dataNascimento": "2000-09-10",
-//                    "celular": "(79)98765-4321",
-//                    "apelido": "Lu",
-//                    "matricula": "ALU2020005"
-//            }
-//      ]
-//        }
-//  ]
-//    }
 
     @Transactional(readOnly = true)
     public ProfessorDisciplinasAlunosDto getProfessorDisciplinasAlunos(String matricula) {
